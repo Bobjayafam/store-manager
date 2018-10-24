b76fd74a4bc5c53dce2a1882119773598251cf20
Bobjayafam <afamjudeo@gmail.com>
2018-10-24 14:16:39 +0100

    feat(api): set up endpoint to get single product
    -setup endpoint to get single product
    
    [Finishes #161447205]

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,5 +3,6 @@ import Product from "../controllers/ProductController";
 const router = express.Router();
 
 router.get("/", Product.getAll);
+router.get("/:product_id", Product.getOne);
 
 export default router;
