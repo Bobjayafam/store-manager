f29e6af867846d4701c013d4bb4a5a518dfd279a
Bobjayafam <afamjudeo@gmail.com>
2018-10-30 17:16:04 +0100

    chore(database): set up postgres database
    - install postgres, pg module and created a connection string
    
    - set up User model
    
    [Finishes #161574563]

---
+++ server/db/UserModel.js
@@ -0,0 +1,12 @@
+const UserModel = {
+  createTable: `
+  DROP TABLE IF EXISTS users;
+  CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    role VARCHAR NOT NULL 
+  );`
+};
+
+export default UserModel;
