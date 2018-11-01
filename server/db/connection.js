9397ba6c681667558648947168052311880ab95d
Bobjayafam <afamjudeo@gmail.com>
2018-11-01 15:56:24 +0100

    change db connection string

--- server/db/connection.js
+++ server/db/connection.js
@@ -6,14 +6,16 @@ dotenv.config();
 // let connectionString = process.env.NODE_ENV === 'test' ? process.env.DATABASE_URL_TEST : process.env.DATABASE_URL;
 
 let connectionString = process.env.DATABASE_URL;
-
+let ssl = false;
 if (process.env.NODE_ENV === 'test') {
   connectionString = process.env.DATABASE_URL_TEST;
 }
-
+if (process.env.NODE_ENV === 'production') {
+  ssl = true;
+}
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl
 });
 
 
