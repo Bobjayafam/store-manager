20e09e2a8922d048a54e46ddb06cb884c9445ecf
Bobjayafam <afamjudeo@gmail.com>
2018-11-01 15:22:46 +0100

    unprotect routes to test database on heroku

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,8 +4,8 @@ import Auth from '../Middleware/Auth';
 
 const router = express.Router();
 
-router.get('/', Auth.verifyToken, ProductController.getAll);
-router.post('/', Auth.isLoggedInAsAdmin, ProductController.add);
+router.get('/', ProductController.getAll);
+router.post('/', ProductController.add);
 router.get('/:id', Auth.isLoggedInAsAdmin, ProductController.getOne);
 router.put('/:id', Auth.isLoggedInAsAdmin, ProductController.update);
 router.delete('/:id', Auth.isLoggedInAsAdmin, ProductController.delete);
