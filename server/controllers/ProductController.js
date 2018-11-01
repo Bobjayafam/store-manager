4d8185ca6307c94a6c189126f248f2c09e501136
Bobjayafam <afamjudeo@gmail.com>
2018-11-01 14:21:36 +0100

    Feat(product rest points): create product endpoints
    [Finishes #161644125]

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -1,44 +1,99 @@
 import ProductModel from '../model/Products';
+import db from '../db/connection';
 
 class ProductController {
   static add(req, res) {
-    const product = ProductModel.addProduct(req.body);
-    return res.status(201).json({
-      success: true,
-      product,
-    });
+    const {
+      name, price, quantity, description, imgUrl,
+    } = req.body;
+    if (!name || !price || !quantity || !description || !imgUrl) {
+      res.status(400).json({
+        success: false,
+        message: 'All fields are required',
+      });
+    }
+    if (typeof price !== 'number' || typeof quantity !== 'number') {
+      res.status(400).json({
+        success: false,
+        message: 'Price can only be a number',
+      });
+    }
+    db.query('INSERT INTO products (name, price, quantity, description, imgUrl) VALUES ($1, $2, $3, $4, $5) RETURNING *', [name, price, quantity, description, imgUrl])
+      .then((product) => {
+        console.log(product);
+        if (product.rowCount > 0) {
+          res.status(200).json({
+            success: true,
+            data: product.rows,
+          });
+        }
+      });
   }
 
   static getAll(req, res) {
-    const products = ProductModel.findAll();
-    return res.status(200).json({
-      success: true,
-      products,
-    });
+    db.query('SELECT * FROM products')
+      .then((products) => {
+        res.status(200).json({
+          success: true,
+          data: products.rows,
+        });
+      }).catch((error) => {
+        res.status(500).json({
+          success: false,
+          message: error.message,
+        });
+      });
   }
 
   static getOne(req, res) {
-    const product = ProductModel.findOne(req.params.productId);
-    return res.status(200).json({
-      success: true,
-      product,
-    });
+    const { id } = req.params;
+    db.query('SELECT * FROM products WHERE id = $1', [parseInt(id, 10)])
+      .then((product) => {
+        console.log(product);
+        if (product.rowCount > 0) {
+          res.status(200).json({
+            success: true,
+            data: product.rows[0],
+          });
+        } else {
+          res.status(404).json({
+            success: false,
+            message: 'Product not found',
+          });
+        }
+      }).catch((error) => {
+        res.status(500).json({
+          success: false,
+          message: error.message,
+        });
+      });
   }
 
   static update(req, res) {
-    const product = ProductModel.findOne(req.params.productId);
-    const updatedProduct = ProductModel.update(req.params.productId, req.body);
-    return res.status(200).json({ success: true, updatedProduct });
+    const {
+      name, price, quantity, description, imgUrl,
+    } = req.body;
+    const id = parseInt(req.params.id, 10);
+
+    db.query('UPDATE products SET name=$1, price=$2, quantity=$3, description=$4, imgUrl=$5 WHERE id=$6', [name, price, quantity, description, imgUrl, id])
+      .then((product) => {
+        res.status(200).json({
+          success: true,
+          message: `successfully updated product with id ${id}`,
+        });
+      });
   }
 
   static delete(req, res) {
-    const product = ProductModel.findOne(req.params.productId);
-    if (!product) {
-      res.status(400).json({ message: 'Product not found' });
-      return;
-    }
-    const deleteResponse = ProductModel.delete(product);
-    res.status(204).send(deleteResponse);
+    let { id } = req.params;
+    id = parseInt(id, 10);
+    db.query('DELETE FROM products WHERE id=$1', [id])
+      .then((result) => {
+        res.status(200).json({
+          success: true,
+          message: `Removed ${result.rowCount} product`,
+        });
+      });
   }
 }
 
